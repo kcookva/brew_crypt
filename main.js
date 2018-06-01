9c5cdd331b7a08ab2800f39b8ec8b1f2eeafe367
kcookvt <kaidencook03@gmail.com>
2018-05-31 20:52:29 -0400

    Added mining blocks

--- main.js
+++ main.js
@@ -1,65 +1,113 @@
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require("crypto-js/sha256");
 
-class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-    }
-
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+//Transactions are important so miners don't continously mine blocks
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
     }
 }
 
-class Blockchain {
+class Block {
+    constructor(timestamp, transactions, previousHash = '') {
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.transactions = transactions;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("BLOCK MINED: " + this.hash);
+    }
+}
+
+
+class Blockchain{
     constructor() {
-        this.chain = [this.createGenesisBlock];
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0, '30/05/2018', "Genesis Block", '0');
+        return new Block(Date.parse("2017-01-01"), [], "0");
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+        block.mineBlock(this.difficulty);
 
+        console.log('Block successfully mined!');
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isChainValid() {
-        for(let i = 1; i < this.chain.length; i++) {
+        for (let i = 1; i < this.chain.length; i++){
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
 
-            if(currentBlock.hash !== currentBlock.calculateHash()) {
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
                 return false;
             }
-            if(currentBlock.previousHash !== previousBlock.hash) {
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
                 return false;
             }
         }
+
         return true;
     }
 }
 
 let brewcrypt = new Blockchain();
-brewcrypt.addBlock(new Block(1, '30/05/2018', {amount: 4}));
-brewcrypt.addBlock(new Block(2, '31/05/2018', {amount: 10}));
-
-console.log('Is blockchain valid? ' + brewcrypt.isChainValid());
-
-//brewcrypt.chain[1].data = {amount: 100}; // returns false because there is no matching hash (this is to test the immutability of this blockchain)
-//brewcrypt.chain[1].hash = brewcrypt.chain[1].calculateHash(); // this is an attempt to recreate the hash and it still returns false (which is good)
-
-console.log('Is blockchain valid? ' + brewcrypt.isChainValid());
+brewcrypt.createTransaction(new Transaction('address1', 'address2', 100));
 
 
-console.log(JSON.stringify(brewcrypt, null, 3));
+for(i = 0; i <= 5; i++) {
+    console.log('\n Starting the miner...');
+    brewcrypt.minePendingTransactions('phils-address');
+
+    console.log('\nBalance of phil is', brewcrypt.getBalanceOfAddress('phils-address'));
+}
